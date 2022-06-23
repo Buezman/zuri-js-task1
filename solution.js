635ef672618eec7d329b8c7afc27f7cbd232e093
Buezman <chibuezenwajiobi@gmail.com>
2022-06-23 22:56:24 +0100

    completed task

---
+++ solution.js
@@ -0,0 +1,7 @@
+const myName = "Nwajiobi Chibueze"
+const height = "5ft 9\""
+const country = "Nigeria"
+
+console.log("NAME:  " + myName)
+console.log("HEIGHT:  " + height)
+console.log("COUNTRY:  " + country)
